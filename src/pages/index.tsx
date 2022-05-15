e3aab5324ebcc03beb65c960441f3d28dec7bd3b
Gabriel Gomes de Almeida <gabrielda@.ciandt.com>
2022-05-14 21:28:25 -0300

    Set general structure for the prj

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
 import { ContentHeader } from '../components/ContentHeader';
+import { CustomerTable } from '../components/CustomerTable';
 
 import styles from './home.module.scss';
 
 
 export default function Home() {
   return (
-    <ContentHeader />
+    <>
+      <ContentHeader />
+      <CustomerTable />
+    </>
     )
 }
 
